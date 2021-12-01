defc28cbfbc1b8236d71509d208d3efdd6cb66ba
Samuel Lucas <samuellucas24383@gmail.com>
2021-12-01 00:51:11 -0300

    scrips

---
+++ Fundamentos do Desenvolvimento Web/Bloco 4: Introdução à JavaScript e Lógica de Programação/4.2 - JavaScript - Array e loop For/scrip5.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// o valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso atribuíssemos o valor 101 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
+
+let higherNumber = numbers[0];
+
+for (let index = 1; index < numbers.length; index += 1) {
+  if (numbers[index] > higherNumber) {
+    higherNumber = numbers[index];
+  }
+}
+
+console.log(higherNumber);
---
+++ Fundamentos do Desenvolvimento Web/Bloco 4: Introdução à JavaScript e Lógica de Programação/4.2 - JavaScript - Array e loop For/scrip9.js
@@ -0,0 +1,3 @@
+for (let index = 0; index < numbers.length; index += 1) {
+    console.log(numbers[index] / 2);
+  };
