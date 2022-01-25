1f34567ac1cad63e1f311bd91fa524d9e9abb692
Samuel Lucas <samuellucas24383@gmail.com>
2022-01-24 21:20:04 -0300

    foi resolvido exercicios

--- Fundamentos-do-Desenvolvimento-Web/Bloco-7:Introducao-a-JavaScript-ES6-e-Testes-Unitarios/7.1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/exercicio1.js
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-7:Introducao-a-JavaScript-ES6-e-Testes-Unitarios/7.1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/exercicio1.js
@@ -1,19 +1,26 @@
-      function testingScope(escopo) {
-        if (escopo === true) {
-          var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-          ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-          console.log(ifScope);
-        } else {
-          var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-          console.log(elseScope);
-        }
-        console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-      }
+  const testingScope = escopo => { 
+    if (escopo === true) { 
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+  }
 
-      testingScope(true);
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-            const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const sortOddsAndEvens = () => {
+  oddsAndEvens[0] = 2;
+  oddsAndEvens[1] = 3;
+  oddsAndEvens[2] = 4;
+  oddsAndEvens[3] = 7;
+  oddsAndEvens[4] = 10;
+  oddsAndEvens[5] = 13;
 
-      // Seu código aqui.
+  return oddsAndEvens;
+}
 
-      console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`); // será necessário alterar essa linha 😉
