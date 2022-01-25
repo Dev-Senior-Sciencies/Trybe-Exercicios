178aad2064d760d9c7714b3de467303c3aa6dd7b
Samuel Lucas <samuellucas24383@gmail.com>
2022-01-24 21:33:30 -0300

    foi resolvido exercicios

--- Fundamentos-do-Desenvolvimento-Web/Bloco-7:Introducao-a-JavaScript-ES6-e-Testes-Unitarios/7.1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/exercicio2.js
+++ Fundamentos-do-Desenvolvimento-Web/Bloco-7:Introducao-a-JavaScript-ES6-e-Testes-Unitarios/7.1-JavaScript-ES6-let-const-arrow-functions-e-template-literals/exercicio2.js
@@ -1,1 +1,18 @@
       longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+
+      const longestWord = text => {
+    let wordArray = text.split(' ');
+    let maxLength = 0;
+    let result = '';
+
+    for (const word of wordArray) {
+        if (word.length > maxLength) {
+            maxLength = word.length;
+            result = word;
+        }
+    }
+
+    return result;
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
